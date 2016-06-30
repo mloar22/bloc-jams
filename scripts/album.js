a0bd4d8c92c1ae65a734c47041a2831415e68f7b
Mitch Loar <mitchloarweb@gmail.com>
2016-06-29 18:24:06 -0600

    CP26 play/pause

--- scripts/album.js
+++ scripts/album.js
@@ -62,7 +62,7 @@ var albumTest = {
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
+     + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
@@ -91,18 +91,27 @@ var albumTest = {
      }
  };
 
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
  
- window.onload = function() {
-     setCurrentAlbum(albumPicasso);
- 
-// If use clicks on the Album Cover it will toggle between albums. 
-  var albums = [albumPicasso, albumMarconi, albumRandom, albumTest];
-   var list = 0;
-  albumImage.onclick = function() {
-    setCurrentAlbum(albums[list]);
-    list++;
-    // when list reaches the last album the next click goes back to 0 index. LOOP? IF Loop?
-  };
-           
+ var songRows = document.getElementsByClassName('album-view-song-item');
 
-};
+
+// Album Button Templates
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
+window.onload = function () {
+  setCurrentAlbum(albumPicasso);
+
+  songListContainer.addEventListener('mouseover', function (event) {
+    if (event.target.parentElement.className === 'album-view-song-item') {
+     event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+    }
+  });
+  for (var i = 0; i < songRows.length; i++) {
+   songRows[i].addEventListener('mouseleave', function (event) {
+     // Selects first child element, which is the song-item-number element
+
+     this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+   });
+ }
+ }
