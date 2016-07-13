47d49758c17e844ccfa98a9028518042173cd47b
Mitch Loar <mitchloarweb@gmail.com>
2016-07-13 00:06:00 -0600

    jQuery CP 30, Collection and Album views

--- scripts/album.js
+++ scripts/album.js
@@ -1,4 +1,3 @@
- // Example Album
  var albumPicasso = {
    title: 'The Colors',
    artist: 'Pablo Picasso',
@@ -123,26 +122,28 @@
    var template =
      '<tr class="album-view-song-item">' + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>' + '  <td class="song-item-title">' + songName + '</td>' + '  <td class="song-item-duration">' + songLength + '</td>' + '</tr>';
 
-   return template;
+   return $(template);
  };
 
- var albumTitle = document.getElementsByClassName('album-view-title')[0];
- var albumArtist = document.getElementsByClassName('album-view-artist')[0];
- var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
- var albumImage = document.getElementsByClassName('album-cover-art')[0];
- var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
 
 
  var setCurrentAlbum = function (album) {
+   var $albumTitle = $('.album-view-title');
+   var $albumArtist = $('.album-view-artist');
+   var $albumReleaseInfo = $('.album-view-release-info');
+   var $albumImage = $('.album-cover-art');
+   var $albumSongList = $('.album-view-song-list');
 
-   albumTitle.firstChild.nodeValue = album.title;
-   albumArtist.firstChild.nodeValue = album.artist;
-   albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+   $albumTitle.text(album.title);
+   $albumArtist.text(album.artist);
+   $albumReleaseInfo.text(album.year + ' ' + album.label);
+   $albumImage.attr('src', album.albumArtUrl);
 
-   albumImage.setAttribute('src', album.albumArtUrl);
-   albumSongList.innerHTML = '';
+   $albumSongList.empty();
    for (var i = 0; i < album.songs.length; i++) {
-     albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+     var $newRow = createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+     $albumSongList.append($newRow);
    }
  };
 
@@ -181,10 +182,10 @@
 
    songListContainer.addEventListener('mouseover', function (event) {
      if (event.target.parentElement.className === 'album-view-song-item') {
-               var songItem = getSongItem(event.target);
-       
-      if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
-            songItem.innerHTML = playButtonTemplate;
+       var songItem = getSongItem(event.target);
+
+       if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
+         songItem.innerHTML = playButtonTemplate;
        }
      }
    });
@@ -192,12 +193,12 @@
      songRows[i].addEventListener('mouseleave', function (event) {
        var songItem = getSongItem(event.target);
        var songItemNumber = songItem.getAttribute('data-song-number');
-       
+
        if (songItemNumber !== currentlyPlayingSong) {
          songItem.innerHTML = songItemNumber;
        }
      });
-     
+
      songRows[i].addEventListener('click', function (event) {
        clickHandler(event.target);
      });
@@ -213,20 +214,20 @@
    }
  };
 
-var getSongItem = function(element) {
-    switch (element.className) {
-        case 'album-song-button':
-        case 'ion-play':
-        case 'ion-pause':
-            return findParentByClassName(element, 'song-item-number');
-        case 'album-view-song-item':
-            return element.querySelector('.song-item-number');
-        case 'song-item-title':
-        case 'song-item-duration':
-            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
-        case 'song-item-number':
-            return element;
-        default:
-            return;
-    }  
-};
+ var getSongItem = function (element) {
+   switch (element.className) {
+   case 'album-song-button':
+   case 'ion-play':
+   case 'ion-pause':
+     return findParentByClassName(element, 'song-item-number');
+   case 'album-view-song-item':
+     return element.querySelector('.song-item-number');
+   case 'song-item-title':
+   case 'song-item-duration':
+     return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+   case 'song-item-number':
+     return element;
+   default:
+     return;
+   }
+ };
--- scripts/landing.js
+++ scripts/landing.js
@@ -1,26 +1,23 @@
- var pointsArray = document.getElementsByClassName('point');
- var animatePoints = function (points) {
-   var revealPoint = function (index) {
-     points[index].style.opacity = 1;
-     points[index].style.transform = "scaleX(1) translateY(0)";
-     points[index].style.msTransform = "scaleX(1) translateY(0)";
-     points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-   }
+var animatePoints = function() {
+  var revealPoint = function() {
+    $(this).css({
+      opacity: 1, 
+      transform: 'scaleX(1) translateY(0)'
+    });
+  };
+  $.each($('.point'), revealPoint);
+};
 
-   for (var i = 0; i < points.length; i++) {
-     revealPoint(i);
-   }
- };
+$(window).load(function() {
+  if ($(window).height() > 950) {
+    animatePoints();
+  }
+  var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200; 
+  
+  $(window).scroll(function(event) {
+    if ($(window).scrollTOp() >= scrollDistance) {
+      animatePoints();
+    }
+  });
 
- window.onload = function () {
-
-   var sellingPoints = document.getElementsByClassName('point')[0];
-   
-   var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-
-   window.addEventListener("scroll", function (event) {
-     if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-       animatePoints(pointsArray);
-     }
-   });
- }
+});
